15c26aeedd2193064251f6513edbba972761d033
Jim Keener <jim@jimkeener.com>
2014-04-15 00:45:52 -0400

    ckpnt?

--- mulic.py
+++ mulic.py
@@ -19,56 +19,147 @@ def parse_helper(prog):
     return stack
 
 class Add:
+    args = 2
     def run(x, y):
-        x = x + y
-        return x
+        x = x[0] + y[0]
+        return (x,x)
     def compile(x, y):
-        free_reg(y)
-        return ("ADD %s, %s" % (x,y), x)
+        free_reg(y[1])
+        return ("ADD %s, %s" % (x[1],y[1]), x[1])
 class Sub:
+    args = 2
     def run(x, y):
         x = x - y
-        return x
+        return (x,x)
     def compile(x, y):
         free_reg(y)
         return ("SUB %s, %s" % (x,y), x)
 class In:
+    args = 1
     def run(x):
-        return int(input("%s> " % x))
+        x=int(input("%s> " % x))
+        return (x,x)
     def compile(x):
         reg = get_reg()
-        return ("IN %s, %s" % (reg, x), reg)
+        return ("IN %s, %s" % (reg, x[0]), reg)
 class Out:
+    args = 2
     def run(x, y):
-        print("%s:\t%d"% (x, y))
+        print("%s:\t%d"% (x, y[0]))
+        return (None, None)
     def compile(x, y):
-        return ("OUT %s, %s" % (x,y),y)
+        return ("OUT %s, %s" % (x,y[1]),y[1])
 
+
+next_jump = 0
+def get_next_jump():
+    global next_jump
+    next_jump += 1
+    return "trgt%d" % next_jump
+
+class If:
+    args = 3
+    def run(ex, t, f):
+        ex = run(ex)
+        if ex[0] == 1:
+            x = run(t)
+            return (x,x)
+        else:
+            x = run(f)
+            return (x,x)
+    def compile(ex, t, f):
+        tgt = get_next_jump();
+        tend = get_next_jump();
+
+        t = compile(t, False)
+        f = compile(f, False)
+        ex = compile(ex, False)
+
+        s = ex[0] + "\n"
+        s += "SUB %s,1\n" % ex[1]
+        s += "BRNE %s\n" % tgt
+        s += t[0] + "\n"
+        s += "JMP %s\n" % tend
+        s += "%s:\n" % tgt
+        s += f[0] + "\n"
+        s += "%s:\n" % tend
+
+        return (s, None)
+
+class Load:
+    def run(x): return (x,x)
+    def compile(x):
+        reg = get_reg()
+        return ("LDI %s, %d" % (reg, x), reg)
+        
 primatives = {
-    "add": (2, Add),
-    "sub": (2, Sub),
-    "in": (1, In),
-    "out": (2, Out),
+    "add": Add,
+    "sub": Sub,
+    "in":  In,
+    "out": Out,
 }
 
-def run(prog):
+macros = {
+    "if": If
+}
+
+def walk_program(prog, execfn, env, printit):
     if type(prog) is list:
-        r = run(prog[0])
-        if r in primatives:
-            cmd = primatives[r]
-            if cmd[0] == 0:
-                return cmd[1].run()
-            elif cmd[0] == 1:
-                return cmd[1].run(run(prog[1]))
-            elif cmd[0] == 2:
-                return cmd[1].run(run(prog[1]), run(prog[2]))
+        r = walk_program(prog[0], execfn, env, printit)
+        if r in env['procs']:
+            cmd = env['procs'][r]
+            ret = [None, None]
+            if cmd.args == 0:
+                ret = getattr(cmd, execfn)()
+            elif cmd.args == 1:
+                ret = getattr(cmd, execfn)(
+                    walk_program(prog[1], execfn, env, printit)
+                )
+            elif cmd.args == 2:
+                ret = getattr(cmd, execfn)(
+                    walk_program(prog[1], execfn, env, printit),
+                    walk_program(prog[2], execfn, env, printit)
+                )
+            elif cmd.args == 3:
+                ret = getattr(cmd, execfn)(
+                    walk_program(prog[1], execfn, env, printit),
+                    walk_program(prog[2], execfn, env, printit),
+                    walk_program(prog[3], execfn, env, printit)
+                )
+            if printit and ret[0] != None:
+                print(ret[0])
+            return ret
+        if r in env['macros']:
+            cmd = env['macros'][r]
+            ret = [None, None]
+            if cmd.args == 0:
+                ret = getattr(cmd, execfn)()
+            elif cmd.args == 1:
+                ret = getattr(cmd, execfn)(
+                    prog[1]
+                )
+            elif cmd.args == 2:
+                ret = getattr(cmd, execfn)(
+                    prog[1],
+                    prog[2],
+                )
+            elif cmd.args == 3:
+                ret = getattr(cmd, execfn)(
+                    prog[1],
+                    prog[2],
+                    prog[3],
+                )
+            if printit and ret[0] != None:
+                print(ret[0])
+            return ret
         return r
     else:
         if prog.isdigit():
-            return int(prog)
+            ld = getattr(Load, execfn)(int(prog))
+            if printit: print(ld[0])
+            return ld
         else:
             return prog
-
 reg_free = [ "r%d" % i for i in range(32) ]
 reg_used = []
 
@@ -83,34 +174,25 @@ def free_reg(reg):
     x = reg_free.append(reg)
     reg_used.remove(reg)
 
-def compile(prog):
-    if type(prog) is list:
-        r = compile(prog[0])
-        if r in primatives:
-            cmd = primatives[r]
-            if cmd[0] == 0:
-                r = cmd[1].compile()
-                print(r[0])
-                return r[1]
-            elif cmd[0] == 1:
-                r = cmd[1].compile(compile(prog[1]))
-                print(r[0])
-                return r[1]
-            elif cmd[0] == 2:
-                r = cmd[1].compile(compile(prog[1]), compile(prog[2]))
-                print(r[0])
-                return r[1]
-        return r
-    else:
-        # Just assume that non numbers are proc names
-        if prog.isdigit():
-            reg = get_reg()
-            print("LDI %s, %d" % (reg, int(prog)))
-            return reg
-        else:
-            return prog
+def run(prog, printit = True):
+    env = {
+        "procs": primatives,
+        "macros": macros,
+    }
+    return walk_program(prog, 'run', env, printit)
+def compile(prog, printit = True):
+    env = {
+        "procs": primatives,
+        "macros": macros,
+    }
+    return walk_program(prog, 'compile', env, printit)
 
 prog = "(out PORTC (add (in PORTB) (add 2 2)))"
+prog = """
+(if (in PORTF) (out PORTC (add 3 (in PORTB)))
+               (out PORTA (add 4 (in PORTC))))
+(out PORTD 42)
+"""
 print(prog)
 prog = parse(prog)
 print(prog)
